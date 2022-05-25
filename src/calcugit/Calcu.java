0b4824d03b0bd9ea843a29cf2b8ffcdb8178f8d0
Manuel Avila <manuel.avila-fernandez@iesruizgijon.com>
2022-05-25 17:39:47 +0200

    "Añadida función modulo división".

--- src/calcugit/Calcu.java
+++ src/calcugit/Calcu.java
@@ -65,5 +65,9 @@ public class Calcu {
         double division=numero1/numero2;
         return division;
     }
+     public double modulo() {
+        double modulo=numero1%numero2;
+        return modulo;
+    }
  
 }
